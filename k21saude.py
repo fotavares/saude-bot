5f4017a6405206a7b41f56ef618c2d502fe02b39
Felipe Tavares <fotavares@users.noreply.github.com>
2019-10-03 17:39:03 -0300

    Versão 0.1

---
+++ k21saude.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+import requests
+import json
+import telepot
+from telepot.loop import MessageLoop
+#from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent
+import constants
+import time
+import sqlite3
+from datetime import datetime
+from pytz import timezone
+import sys
+
+
+BASE_NAME = 'banco.saude.db'
+def insertResposta(user_id, chat_id,message_id,timestamp,response):
+	with sqlite3.connect(BASE_NAME) as con:
+		cursor = con.cursor()
+		sql = 'INSERT INTO resposta values (?,?,?,?,?)'
+		cursor.execute(sql,[(chat_id),(user_id),(message_id),(timestamp),(response)])
+		con.commit()
+
+def insertQuestion(id,perg,timestamp, chat_id):
+	with sqlite3.connect(BASE_NAME) as con:
+		cursor = con.cursor()
+		sql = 'INSERT INTO pergunta values (?,?,?,?,?)'
+		cursor.execute(sql,[(0),(id),(perg),(timestamp),(chat_id)])
+		con.commit()
+
+def getTimes(chat_id):
+	with sqlite3.connect(BASE_NAME) as con:
+		cursor = con.cursor()
+		sql = 'SELECT distinct timestamp FROM pergunta where chat_id=' + str(chat_id) + ' ORDER BY timestamp DESC'
+		cursor.execute(sql)
+		data = cursor.fetchmany(7)
+		return data
+
+def getChats():
+	with sqlite3.connect(BASE_NAME) as con:
+		cursor = con.cursor()
+		sql = 'SELECT distinct chat_id FROM pergunta'
+		cursor.execute(sql)
+		data = cursor.fetchmany()
+		return data
+
+def isPerguntaValida(chat_id,message_id):
+	with sqlite3.connect(BASE_NAME) as con:
+		cursor = con.cursor()
+		sql = 'SELECT 1 FROM pergunta where chat_id=' + str(chat_id) + ' and message_id = '+ str(message_id) +' ORDER BY timestamp DESC'
+		cursor.execute(sql)
+		data = cursor.fetchone()
+		return (data[0] == 1)
+
+def getResult(timestamp, chat_id):
+	with sqlite3.connect(BASE_NAME) as con:
+		cursor = con.cursor()
+		sql = '''
+		select resposta.user_id,resposta.message_id, pergunta.question, response , resposta."timestamp" from resposta
+		inner join pergunta on resposta."timestamp" == pergunta."timestamp" and resposta.message_id == pergunta.message_id
+		and resposta.chat_id == pergunta.chat_id
+		where resposta."timestamp" = '''  + timestamp + ''' and resposta.chat_id = ''' + str(chat_id) + ''' 
+		order by resposta.user_id,resposta.message_id'''
+		cursor.execute(sql)
+		data = cursor.fetchall()
+		return data
+		
+def handle(msg):
+	content_type, chat_type, chat_id = telepot.glance(msg)
+	#print(msg)
+
+	data = datetime.now(timezone('Brazil/East')).strftime('%Y%m%d')
+
+	if '/saude' in msg['text']:
+		stamps = getTimes(chat_id)
+		send_question = True if (len(stamps) == 0) else True if (str(data) not in stamps[0]) else False
+		if (send_question):
+			enviada = bot.sendMessage(chat_id,"Quantos dias você trabalhou fora do horário de trabalho essa semana?")
+			insertQuestion(enviada['message_id'],enviada['text'],data,chat_id)
+		else:
+				bot.sendMessage(chat_id,"Já foi enviada uma enquete hoje")
+
+	if '/resultado' in msg['text'] :
+		stamps = getTimes(chat_id)
+		if(len(stamps) == 0):
+			bot.sendMessage(chat_id,"Nenhuma pergunta foi feita ainda. Envie /saude antes")
+		else:
+			dataRecuperada = datetime.strptime(stamps[0][0], "%Y%m%d")
+			
+			resultados = getResult(stamps[0][0],chat_id)
+			if(len(resultados) == 0):
+				bot.sendMessage(chat_id,"Sem respostas até o momento!")
+			else:
+				#print(resultados)
+				id_usuario_anterior = 0
+				msg_result = ''
+				flPrintPergunta = False
+				for result in resultados:
+					user_id = result[0]
+					text = result[2]
+					resp = result[3]
+					user = bot.getChatMember(chat_id,user_id)
+
+					if not flPrintPergunta:
+						msg_result = msg_result + dataRecuperada.strftime("%d/%m/%Y") + '\n\n'
+						msg_result = msg_result + text + ' \n'
+						flPrintPergunta = True
+
+					if(id_usuario_anterior != user_id):
+						msg_result = msg_result + '\n' + user['user']['first_name'] + ' ('+user['user']['username']+') :  ' + resp 
+					id_usuario_anterior = user_id
+				
+				bot.sendMessage(chat_id,msg_result)
+		#	bot.sendMessage(chat_id, "Escolha a data para o resultado:",reply_markup=keybResult)
+
+	if 'reply_to_message' in msg:
+		id_pergunta = msg['reply_to_message']['message_id']
+		from_pergunta = msg['reply_to_message']['from']['username']
+		user_id = msg['from']['id']
+		resposta = msg['text']
+
+		me = bot.getMe()
+		if from_pergunta == me['username']:
+			#é uma pergunta feita pelo bot?
+			if isPerguntaValida(chat_id,id_pergunta):
+				#grava a resposta
+				insertResposta(user_id, chat_id,id_pergunta,data,resposta)
+	
+
+
+bot = telepot.Bot('845427417:AAEe6lm496Xja7t1TnLqPHFTPk8p31KqATI')
+if len(sys.argv) > 1:
+	data = datetime.now(timezone('Brazil/East')).strftime('%Y%m%d')
+	for chat_id in getChats():
+		enviada = bot.sendMessage(chat_id[0],"Quantos dias você trabalhou fora do horário de trabalho essa semana?")
+		insertQuestion(enviada['message_id'],enviada['text'],data,chat_id[0])
+		print("Mensagem Enviada via Cron")
+else:
+	MessageLoop(bot, {'chat':handle}).run_as_thread()
+
+print ('Executando Saude Magrathea...')
+
+while 1:
+	time.sleep(10)
+
+
