15af5955ffbef0b34f7b26263d74292e8f8a2413
Felipe Tavares <fotavares@users.noreply.github.com>
2019-10-25 14:53:58 -0300

    Versao 0.2
    Ajuste em query
    Preparação para ser executado via Cron Job

--- k21saude.py
+++ k21saude.py
@@ -3,7 +3,6 @@ import requests
 import json
 import telepot
 from telepot.loop import MessageLoop
-#from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent
 import constants
 import time
 import sqlite3
@@ -38,9 +37,10 @@ def getTimes(chat_id):
 def getChats():
 	with sqlite3.connect(BASE_NAME) as con:
 		cursor = con.cursor()
-		sql = 'SELECT distinct chat_id FROM pergunta'
+		sql = 'SELECT distinct chat_id FROM pergunta '
 		cursor.execute(sql)
-		data = cursor.fetchmany()
+		data = cursor.fetchall()
+		#print(data)
 		return data
 
 def isPerguntaValida(chat_id,message_id):
@@ -71,45 +71,10 @@ def handle(msg):
 	data = datetime.now(timezone('Brazil/East')).strftime('%Y%m%d')
 
 	if '/saude' in msg['text']:
-		stamps = getTimes(chat_id)
-		send_question = True if (len(stamps) == 0) else True if (str(data) not in stamps[0]) else False
-		if (send_question):
-			enviada = bot.sendMessage(chat_id,"Quantos dias você trabalhou fora do horário de trabalho essa semana?")
-			insertQuestion(enviada['message_id'],enviada['text'],data,chat_id)
-		else:
-				bot.sendMessage(chat_id,"Já foi enviada uma enquete hoje")
+		EnviaPergunta(chat_id, data)
 
 	if '/resultado' in msg['text'] :
-		stamps = getTimes(chat_id)
-		if(len(stamps) == 0):
-			bot.sendMessage(chat_id,"Nenhuma pergunta foi feita ainda. Envie /saude antes")
-		else:
-			dataRecuperada = datetime.strptime(stamps[0][0], "%Y%m%d")
-			
-			resultados = getResult(stamps[0][0],chat_id)
-			if(len(resultados) == 0):
-				bot.sendMessage(chat_id,"Sem respostas até o momento!")
-			else:
-				#print(resultados)
-				id_usuario_anterior = 0
-				msg_result = ''
-				flPrintPergunta = False
-				for result in resultados:
-					user_id = result[0]
-					text = result[2]
-					resp = result[3]
-					user = bot.getChatMember(chat_id,user_id)
-
-					if not flPrintPergunta:
-						msg_result = msg_result + dataRecuperada.strftime("%d/%m/%Y") + '\n\n'
-						msg_result = msg_result + text + ' \n'
-						flPrintPergunta = True
-
-					if(id_usuario_anterior != user_id):
-						msg_result = msg_result + '\n' + user['user']['first_name'] + ' ('+user['user']['username']+') :  ' + resp 
-					id_usuario_anterior = user_id
-				
-				bot.sendMessage(chat_id,msg_result)
+		CarregaResultado(chat_id)
 		#	bot.sendMessage(chat_id, "Escolha a data para o resultado:",reply_markup=keybResult)
 
 	if 'reply_to_message' in msg:
@@ -124,22 +89,78 @@ def handle(msg):
 			if isPerguntaValida(chat_id,id_pergunta):
 				#grava a resposta
 				insertResposta(user_id, chat_id,id_pergunta,data,resposta)
+
+def EnviaPergunta(chat_id, data):
+    stamps = getTimes(chat_id)
+    send_question = True if (len(stamps) == 0) else True if (str(data) not in stamps[0]) else False
+    if (send_question):
+        enviada = bot.sendMessage(chat_id,"Quantos dias você trabalhou fora do horário de trabalho essa semana?")
+        insertQuestion(enviada['message_id'],enviada['text'],data,chat_id)
+    else:
+        bot.sendMessage(chat_id,"Já foi enviada uma enquete hoje")
+
+def CarregaResultado(chat_id):
+    stamps = getTimes(chat_id)
+    if(len(stamps) == 0):
+            bot.sendMessage(chat_id,"Nenhuma pergunta foi feita ainda. Envie /saude antes")
+    else:
+            dataRecuperada = datetime.strptime(stamps[0][0], "%Y%m%d")
+
+            resultados = getResult(stamps[0][0],chat_id)
+            if(len(resultados) == 0):
+                    bot.sendMessage(chat_id,"Sem respostas até o momento!")
+            else:
+                    print(resultados)
+                    id_usuario_anterior = 0
+                    msg_result = ''
+                    flPrintPergunta = False
+                    for result in resultados:
+                            user_id = result[0]
+                            text = result[2]
+                            resp = result[3]
+                            user = bot.getChatMember(chat_id,user_id)
+
+                            if not flPrintPergunta:
+                                    msg_result = msg_result + dataRecuperada.strftime("%d/%m/%Y") + '\n\n'
+                                    msg_result = msg_result + text + ' \n'
+                                    flPrintPergunta = True
+
+                            if(id_usuario_anterior != user_id):
+                                    msg_result = msg_result + '\n' + user['user']['first_name'] + ' ('+user['user']['username']+') :  ' + resp 
+                            id_usuario_anterior = user_id
+
+                    bot.sendMessage(chat_id,msg_result)
 	
 
 
-bot = telepot.Bot('845427417:AAEe6lm496Xja7t1TnLqPHFTPk8p31KqATI')
+bot = telepot.Bot('')
 if len(sys.argv) > 1:
 	data = datetime.now(timezone('Brazil/East')).strftime('%Y%m%d')
-	for chat_id in getChats():
-		enviada = bot.sendMessage(chat_id[0],"Quantos dias você trabalhou fora do horário de trabalho essa semana?")
-		insertQuestion(enviada['message_id'],enviada['text'],data,chat_id[0])
-		print("Mensagem Enviada via Cron")
+	if(sys.argv[1]) == 'Pergunta':
+		for chat_id in getChats():
+			try:
+				#print(chat_id[0])
+				bot.sendChatAction(chat_id[0],'typing')
+			except:
+				print(chat_id[0] + " indisponível")
+				pass
+			else:
+				EnviaPergunta(chat_id[0], data)
+				print("Mensagem Enviada via Cron")
+	elif (sys.argv[1]) == 'Resposta':
+		for chat_id in getChats():
+			try:
+				#print(chat_id[0])
+				bot.sendChatAction(chat_id[0],'typing')
+			except:
+				print(chat_id[0] + " indisponível")
+				pass
+			else:
+				CarregaResultado(chat_id[0])
 else:
 	MessageLoop(bot, {'chat':handle}).run_as_thread()
-
-print ('Executando Saude Magrathea...')
-
-while 1:
-	time.sleep(10)
+	print ('Executando Saude Magrathea...')
+	while 1:
+		time.sleep(10)
 
 
